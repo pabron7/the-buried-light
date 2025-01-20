a72d76ee66a5446b11a759215e474b24ee6a955f
Alp Kurt <krtalp@gmail.com>
2025-01-20 13:21:33 +0300

    Update WebGL build 20-01-2025

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
-const cacheName = "DefaultCompany-The Buried Light-0.1";
+const cacheName = "lupi-TheBuriedLight-0.1";
 const contentToCache = [
     "Build/TheBuriedLight-WebGL.loader.js",
-    "Build/TheBuriedLight-WebGL.framework.js",
-    "Build/TheBuriedLight-WebGL.data",
-    "Build/TheBuriedLight-WebGL.wasm",
+    "Build/TheBuriedLight-WebGL.framework.js.gz",
+    "Build/TheBuriedLight-WebGL.data.gz",
+    "Build/TheBuriedLight-WebGL.wasm.gz",
     "TemplateData/style.css"
 
 ];
